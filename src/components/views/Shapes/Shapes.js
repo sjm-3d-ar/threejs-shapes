e216860112d726bc7b06b5f30233665b4045a75a
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-07-22 11:49:16 -0400

    create renderer, scene, camera, object (mesh, geometry, material), and display the cube

--- src/components/views/Shapes/Shapes.js
+++ src/components/views/Shapes/Shapes.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-param-reassign */
 import React, { useRef, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import * as THREE from "three";
 
 const useStyles = makeStyles({
   root: {
@@ -14,7 +15,30 @@ const Shapes = () => {
 
   const canvas = useRef();
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const renderer = new THREE.WebGLRenderer({ canvas: canvas.current });
+
+    const fov = 75;
+    const aspect = 2; // w / h
+    const near = 0.1;
+    const far = 5;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 2;
+
+    const scene = new THREE.Scene();
+
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+    const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+
+    const cube = new THREE.Mesh(geometry, material);
+
+    scene.add(cube);
+    renderer.render(scene, camera);
+  }, []);
 
   return <canvas className={classes.root} ref={canvas} />;
 };
